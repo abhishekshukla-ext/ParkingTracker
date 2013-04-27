d68b46247809487f2a324f0a91cce87f5f87a1d7
Abbey S <abbey0007@gmail.com>
2013-04-27 05:44:48 +0530

    first commit

---
+++ ParkingTracker/ParkingTracker/AddNotification.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Navigation;
+using Microsoft.Phone.Scheduler;
+
+namespace ParkingTracker
+{
+    public partial class AddNotification
+    {
+        public AddNotification()
+        {
+            InitializeComponent();
+        }
+
+        private void ApplicationBarSaveButtonClick(object sender, EventArgs e)
+        {
+            String name = Guid.NewGuid().ToString();
+
+            var time = DateTime.Now;
+
+            if (expirationTimePicker.Value != null)
+                time = (DateTime) expirationTimePicker.Value;
+            DateTime expirationTime = time;
+            //TODO : Replace the begin time with ExpirationTime-15
+            DateTime beginTime = expirationTime.Subtract(new TimeSpan(0, 15, 0));
+            //DateTime beginTime = expirationTime;
+
+            if (DateTime.Now > beginTime)
+            {
+                MessageBox.Show("Parking meter already expired or about to expire within next 15 minutes.");
+                return;
+            }
+
+            // Create a URI for the page that will be launched if the user
+            // taps on the reminder. Use query string parameters to pass 
+            // content to the page that is launched.
+            
+            var navigationUri = new Uri("/MainPage.xaml",UriKind.Relative);
+
+
+            Reminder reminder = new Reminder(name);
+            reminder.Title = "Parking meter expiring title";
+            reminder.Content = "Parking meter expiring content";
+            reminder.BeginTime = beginTime;
+            reminder.ExpirationTime = expirationTime;
+            reminder.RecurrenceType = RecurrenceInterval.None;
+            reminder.NavigationUri = navigationUri;
+
+            // Register the reminder with the system.
+            ScheduledActionService.Add(reminder);
+
+            NavigationService.GoBack();
+        }
+    }
+}
---
+++ ParkingTracker/ParkingTracker/MainPage.xaml.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Threading;
+using Microsoft.Phone.Scheduler;
+
+namespace ParkingTracker
+{
+    public partial class MainPage
+    {
+        IEnumerable<ScheduledNotification> _notifications;
+        private DispatcherTimer _dispatcherTimer;
+        private DateTime EndTime { get; set; }
+
+        protected override void OnNavigatedTo(System.Windows.Navigation.NavigationEventArgs e)
+        {
+            EndTime = DateTime.MinValue;
+            _dispatcherTimer = null;
+            ResetItemsList();
+        }
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ApplicationBarAddButtonClick(object sender, EventArgs e)
+        {
+            if (_notifications != null && _notifications.Count<ScheduledNotification>() > 0)
+            {
+                MessageBox.Show("Parking time tracker already exists. Please delete this one to add a new one");
+            }
+            else
+            {
+                NavigationService.Navigate(new Uri("/AddNotification.xaml", UriKind.RelativeOrAbsolute));
+            }
+        }
+
+        private void ResetItemsList()
+        {
+            _notifications = ScheduledActionService.GetActions<ScheduledNotification>();
+
+            if (_notifications.Count<ScheduledNotification>() > 0)
+            {
+                var note = _notifications.First<ScheduledNotification>();
+                timeSpan.Value = ((Microsoft.Phone.Scheduler.ScheduledAction) (note)).ExpirationTime.TimeOfDay -
+                                      DateTime.Now.TimeOfDay;
+                BeginTime.Text = ((Microsoft.Phone.Scheduler.ScheduledAction) (note)).BeginTime.TimeOfDay.ToString();
+                ExpirationTime.Text = ((Microsoft.Phone.Scheduler.ScheduledAction)(note)).ExpirationTime.TimeOfDay.ToString();
+
+                if (_dispatcherTimer == null)
+                {
+                    _dispatcherTimer = new DispatcherTimer { Interval = TimeSpan.FromMilliseconds(100) };
+                    _dispatcherTimer.Tick += DispatcherTimerTick;
+                }
+
+                if (EndTime == DateTime.MinValue)
+                {
+                    if (timeSpan.Value != null) EndTime = DateTime.Now + (TimeSpan)timeSpan.Value;
+                }
+
+                _dispatcherTimer.Start();
+
+                EmptyTextBlock.Visibility = Visibility.Collapsed;
+                timespanPanel.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                EmptyTextBlock.Visibility = Visibility.Visible;
+            }
+        }
+
+        void DispatcherTimerTick(object sender, EventArgs e)
+        {
+            var remaining = EndTime - DateTime.Now;
+            var remainingSeconds = (int)remaining.TotalSeconds;
+            timeSpan.Value = TimeSpan.FromSeconds(remainingSeconds);
+
+            if (remaining.TotalSeconds <= 0)
+            {
+                _dispatcherTimer.Stop();
+                ScheduledActionService.Remove(_notifications.First<ScheduledNotification>().Name);
+                EndTime = DateTime.MinValue;
+                timeSpan.Value = new TimeSpan();
+                timespanPanel.Visibility = Visibility.Collapsed;
+                EmptyTextBlock.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void ApplicationBarDeleteButtonClick(object sender, EventArgs e)
+        {
+            if (_notifications.Count<ScheduledNotification>() > 0)
+            {
+                ScheduledActionService.Remove(_notifications.First<ScheduledNotification>().Name);
+                _notifications = null;
+                EndTime = DateTime.MinValue;
+                timeSpan.Value = new TimeSpan();
+                timespanPanel.Visibility = Visibility.Collapsed;
+                EmptyTextBlock.Visibility = Visibility.Visible;
+                if (_dispatcherTimer != null)
+                {
+                    _dispatcherTimer.Stop();
+                }
+            }
+        }
+    }
+}
